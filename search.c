36812fa3aec900379c414621201ab9f57ef1120b
Marko Rupnik <mrupnik@biosistemika.com>
2024-12-25 10:51:08 +0100

    1b325bf

--- search.c
+++ search.c
@@ -1229,7 +1229,7 @@ moves_loop: // When in check search starts from here
     if (    depth >= 3
         &&  moveCount > 1 + 2 * rootNode
         && (   !captureOrPromotion
-            || cutNode
+            || (cutNode && (ss-1)->moveCount > 1)
             || !ss->ttPv)
         && (!PvNode || ss->ply > 1 || pos->threadIdx % 4 != 3))
     {
