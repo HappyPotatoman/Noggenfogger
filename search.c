6e9a81472ff33f456cffa1761a81f20a32ee5a68
Marko Rupnik <mrupnik@biosistemika.com>
2024-12-28 20:19:42 +0100

    6a6e32d

--- search.c
+++ search.c
@@ -916,8 +916,8 @@ INLINE Value search_node(Position *pos, Stack *ss, Value alpha, Value beta,
   }
 
   // Step 10. If the position is not in TT, decrease depth by 2
-  if (PvNode && depth >= 6 && !ttMove)
-    depth -= 2;
+  if (PvNode && !ttMove)
+    depth -= 2 + 2 * (ss->ttHit && tte_depth(tte) >= depth);
 
 moves_loop: // When in check search starts from here
 
