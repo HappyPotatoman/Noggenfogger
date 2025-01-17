33a3c318fcc8510352808b36a9db6ef6288e1a58
Marko Rupnik <mrupnik@biosistemika.com>
2025-01-17 11:48:21 +0100

    30fdbf4

--- search.c
+++ search.c
@@ -898,8 +898,15 @@ INLINE Value search_node(Position *pos, Stack *ss, Value alpha, Value beta,
   }
 
   // Step 10. If the position is not in TT, decrease depth by 2
-  if (PvNode && depth >= 6 && !ttMove)
+  if ( PvNode 
+    && depth >= 6 
+    && !ttMove)
     depth -= 2;
+
+  if ( cutNode
+    && depth >= 9
+    && !ttMove)
+    depth--;
 
 moves_loop: // When in check search starts from here
 
