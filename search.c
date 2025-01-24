a81e4aa85e1e62b7259cb62616216e15c90d31a8
Marko Rupnik <mrupnik@biosistemika.com>
2025-01-24 13:26:51 +0100

    af050e5

--- search.c
+++ search.c
@@ -999,7 +999,7 @@ moves_loop: // When in check search starts from here
         // Futility pruning: parent node
         if (    !inCheck
             &&  lmrDepth < 8
-            &&  ss->staticEval + 172 + 145 * lmrDepth + history / 128 <= alpha)
+            &&  ss->staticEval + 142 + 139 * lmrDepth + history / 64 <= alpha)
           continue;
 
         // Prune moves with negative SEE at low depths and below a decreasing
